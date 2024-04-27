32759d8d0e775847ec1564888839126acecdaf72
Fuentes Aubrien <143090289+Rebisco-Biscuit@users.noreply.github.com>
2024-04-27 19:57:42 +0800

    Update Linked_lists1.c

--- that_dawg/linked_lists/Linked_lists1.c
+++ that_dawg/linked_lists/Linked_lists1.c
@@ -1,6 +1,24 @@
-/* Fuck it, Lets to linked lists skip 
+/* Fuck it, Lets skip to linked lists: 
 
-https://www.w3resource.com/cpp-exercises/linked_list/index.php
-https://mathcenter.oxford.emory.edu/site/cs171/probSetLinkedLists/
+
+Sabaw:
+
+https://www.w3resource.com/c-programming-exercises/linked_list/index.php
+https://www.cs.ucr.edu/~zhiyunq/teaching/cs153/lectures/C-tutorial/exercises-linked-list.html
 
 */
+
+/* 
+1. Write a program in C to create and display a Singly Linked List.
+Test Data :
+Input the number of nodes : 3
+Input data for node 1 : 5
+Input data for node 2 : 6
+Input data for node 3 : 7
+Expected Output :
+
+ Data entered in the list :                                                                                   
+ Data = 5                                                                                                     
+ Data = 6                                                                                                     
+ Data = 7 
+ */
