52ebd751c90ac3518485374a73454ea8924fdc9f
Fuentes Aubrien <143090289+Rebisco-Biscuit@users.noreply.github.com>
2024-04-28 13:15:47 +0800

    Update Linked_lists1.c

--- that_dawg/linked_lists/Linked_lists1.c
+++ that_dawg/linked_lists/Linked_lists1.c
@@ -22,3 +22,52 @@ Expected Output :
  Data = 6                                                                                                     
  Data = 7 
  */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+ 	int num;
+	struct node *link;
+} *node1;
+
+void createNode(int);
+void display();
+
+int main (){
+	int size;
+
+	printf("Input the number of nodes: );
+	scanf("%d", &size);
+
+	createNode(size);
+	diplay();
+}
+
+void createNode(int size){
+	struct node *head, *current, *newNode;
+	int num;
+
+	if(head==NULL){
+		printf("Cannot create an empty list.");
+	} else {
+		head=malloc(sizeof(struct node));
+	}
+
+	printf("Input data for node 1: ");
+	scanf("%d", &num);
+
+	head->num = num;
+	head->link=NULL;
+	current=head;
+
+	for(int i=2; i<size; i++){
+		newNode=malloc(sizeof(struct node));
+		
+		printf("Input data for node %d", i);
+		scanf("%d", &num);
+
+		newNode->num = num;
+		newNode->link = NULL;
+
